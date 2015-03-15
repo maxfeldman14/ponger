de2d94ecd5c23c9653239c59220eca7b740a0155
Maxwell Feldman <m.feldman@mfeldman-ltm.internal.salesforce.com>
2015-03-15 13:45:14 -0700

    updated for 2 client support

--- client.js
+++ client.js
@@ -1,15 +1,27 @@
 var readline = require('readline'),
 socketio = require('socket.io-client');
-// util = require('util');
  
+// cmd line arg of the host
 var socket = socketio.connect(process.argv[2]);
+
+// cmd line arg of which player we are
+var playerNum = process.argv[3];
 var rl = readline.createInterface(process.stdin, process.stdout);
 
+socket.on(playerNum, function (data) {
+      console.log('other player sent an update');
+      console.log(data.update);
+});
+
 rl.on('line', function (line) {
-    socket.emit('send', { message: line });
+    socket.emit(playerNum, { update: line });
     rl.prompt(true);
 });
 
-socket.on('message', function (data) {
-    console.log(data.message);
+/*
+// listen for an assignment of player number
+socket.on('player', function (data) {
+    playerNum = data.number;
+    console.log("assigned " + playerNum.toString());
 });
+*/
